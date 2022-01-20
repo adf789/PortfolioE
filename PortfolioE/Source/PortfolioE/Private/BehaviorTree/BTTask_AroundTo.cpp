3999c7ac8d321799770e88b1f8fcd26c4474f7ea
adf789 <rlarudgks888@naver.com>
2022-01-20 23:15:30 +0900

    몬스터 ai 랜덤으로 주위 회전하는 노드 추가(50%)

--- PortfolioE/Source/PortfolioE/Private/BehaviorTree/BTTask_AroundTo.cpp
+++ PortfolioE/Source/PortfolioE/Private/BehaviorTree/BTTask_AroundTo.cpp
@@ -1,14 +1,54 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BTTask_AroundTo.h"
+#include "AIController.h"
+#include "POEMonster_Base.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "DrawDebugHelpers.h"
 
 UBTTask_AroundTo::UBTTask_AroundTo() {
-
+	NodeName = TEXT("AroundToTarget");
+	//bNotifyTick = true;
+	IsSetDistance = false;
 }
 
 EBTNodeResult::Type UBTTask_AroundTo::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
 	EBTNodeResult::Type result = Super::ExecuteTask(OwnerComp, NodeMemory);
 
-	return result;
+	AActor* Target = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(BlackboardKey.SelectedKeyName));
+	CHECKRETURN(Target == nullptr, EBTNodeResult::Failed);
+
+	float RandomXDistance = MaxMovableRadius;
+	APOEMonster_Base* ControllingPawn = Cast<APOEMonster_Base>(OwnerComp.GetAIOwner()->GetPawn());
+	CHECKRETURN(ControllingPawn == nullptr, EBTNodeResult::Failed);
+	/*if (ControllingPawn == nullptr) RandomXDistance = 1;
+	else RandomXDistance = ControllingPawn->GetAttackDistance();*/
+
+	FVector TargetLocation = Target->GetActorLocation();
+
+	FNavLocation ResultLocation;
+	UNavigationSystem* NaviSystem = GetWorld()->GetNavigationSystem();
+	CHECKRETURN(NaviSystem == nullptr, EBTNodeResult::Failed);
+	bool bResult = NaviSystem->GetRandomPointInNavigableRadius(TargetLocation, RandomXDistance, ResultLocation);
+	CHECKRETURN(!bResult, EBTNodeResult::Failed);
+
+	FinalToLocation = ResultLocation.Location;
+	//FinalToLocation.Z = ControllingPawn->GetActorLocation().Z;
+	UNavigationSystem::SimpleMoveToLocation(OwnerComp.GetAIOwner(), FinalToLocation);
+	//IsSetDistance = true;
+	return EBTNodeResult::Succeeded;
+}
+
+void UBTTask_AroundTo::TickTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+
+	if (IsSetDistance) {
+		float DestinationDistance = FVector::Dist(OwnerComp.GetAIOwner()->GetPawn()->GetActorLocation(), FinalToLocation);
+		if (DestinationDistance <= 80.0f) IsSetDistance = false;
+	}else {
+		IsSetDistance = false;
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}
 }
--- PortfolioE/Source/PortfolioE/Public/BehaviorTree/BTTask_AroundTo.h
+++ PortfolioE/Source/PortfolioE/Public/BehaviorTree/BTTask_AroundTo.h
@@ -18,8 +18,20 @@ public:
 	UBTTask_AroundTo();
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setting)
-	int32 MaxRandNumber;
+
+protected:
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+	struct FBlackboardKeySelector BlackboardKey;
+
+	UPROPERTY(VisibleAnywhere, Category = Blackboard)
+	FVector FinalToLocation;
+
+	UPROPERTY(VisibleAnywhere, Category = Blackboard)
+	bool IsSetDistance;
+
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+	float MaxMovableRadius;
 };
